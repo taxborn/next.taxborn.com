0d6d201485db0781461c786630bd3520c6ca5191
Braxton Fair <hello@taxborn.com>
2023-02-05 22:26:51 -0600

    wip

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,11 @@
 module.exports = {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			backgroundImage: {
+			  'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
+			}
+		}
 	},
 	plugins: [
 		require('@catppuccin/tailwindcss')({
